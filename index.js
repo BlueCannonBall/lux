dd92acf2f2e697ad28dc915feb3f733aca315667
BlueCannonBall <mmttiizz100@gmail.com>
2024-09-22 17:32:48 -0400

    Remove BROKEN fullscreen feature

--- index.js
+++ index.js
@@ -101,17 +101,6 @@ class SetupForm {
         this.naturalTouchScrollingCheckboxLabelText = document.createTextNode("Natural touch scrolling");
         this.naturalTouchScrollingCheckboxLabel.appendChild(this.naturalTouchScrollingCheckboxLabelText);
 
-        this.fullscreenCheckboxLabel = document.createElement("label");
-        this.fullscreenCheckboxLabel.style.marginBottom = "var(--pico-spacing)";
-        this.inner.appendChild(this.fullscreenCheckboxLabel);
-
-        this.fullscreenCheckbox = document.createElement("input");
-        this.fullscreenCheckbox.type = "checkbox";
-        this.fullscreenCheckboxLabel.appendChild(this.fullscreenCheckbox);
-
-        this.fullscreenCheckboxLabelText = document.createTextNode("Fullscreen");
-        this.fullscreenCheckboxLabel.appendChild(this.fullscreenCheckboxLabelText);
-
         this.submitButton = document.createElement("button");
         this.submitButton.type = "submit";
         this.submitButton.innerText = "Login";
@@ -152,7 +141,6 @@ class SetupForm {
         streamingWindow.startStreaming(
             this.addressInput.value,
             this.passwordInput.value,
-            this.fullscreenCheckbox.checked,
         );
         this.inner.replaceWith(streamingWindow.inner);
     }
@@ -180,7 +168,7 @@ class StreamingWindow {
         this.inner.style.alignItems = "center";
     }
 
-    async startStreaming(address, password, fullscreen = false) {
+    async startStreaming(address, password) {
         this.inner.ariaBusy = true;
         this.inner.innerText = "Connecting...";
         
@@ -189,8 +177,6 @@ class StreamingWindow {
             video: false,
         });
         stream.getTracks().forEach(track => track.stop());
-
-        if (fullscreen) this.inner.requestFullscreen();
 
         this.conn = new RTCPeerConnection({
             iceServers: [
