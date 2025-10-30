55eeeb3666821ee249d1216a71d9b124a8eeebf8
BlueCannonBall <mmttiizz100@gmail.com>
2025-10-30 14:29:51 -0400

    Enable offline PWA

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "static-cache-v1";
+const ASSETS_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/pico.classless.min.css",
+    "/index.js",
+    "/manifest.json",
+    "/favicon.ico",
+    "/icon.png",
+    "/apple-touch-icon.png",
+    "/mouse.png",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => cachedResponse || fetch(event.request))
+    );
+});
